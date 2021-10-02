ca632e24ad93e20963e620f99eb4421ef173e26a
Przemek Kawecki Toshiba <przemek.kawecki@gmail.com>
2021-10-02 10:59:39 +0200

    dataStore loaded, project working

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './styles/global.scss';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import {Provider} from 'react-redux';
+import store from './redux/store'; 
+
+ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('app'));
 
