ebaba235fe5ea185078e88e6c8f89752d133a695
Przemek Kawecki Toshiba <przemek.kawecki@gmail.com>
2021-10-08 19:36:00 +0200

    List link imported

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import styles from './App.scss';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import Faq from '../Faq/Faq';
 import MainLayout from '../MainLayout/MainLayout';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 const App = () => (
 
   <BrowserRouter>
     <MainLayout>
-      <Switch>
+      <AnimatedSwitch 
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className= {styles.switchWrapper}
+      >
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={Faq} />
-      </Switch>
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
   
